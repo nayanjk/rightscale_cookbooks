2b97fe56ebe98e8b0df28030a1062f1371af7c4c
nayan kalita <nayan.kalita@inmobi.com>
2012-06-14 13:02:31 +0530

    adding fast-cgi

---
+++ cookbooks/inmobi_fastcgi/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: inmobi_fastcgi
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+case node[:platform]
+when "ubuntu","debian"
+  package "php5" do
+    action :install
+  end
+end
+
+case node[:platform]
+when "ubuntu","debian"
+  package "php5-cgi" do
+    action :install
+  end
+end
+
+template "/etc/init.d/php-cgi" do
+  source "php-cgi.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+service "php-cgi" do
+  action :restart
+end
