204abec4517ef9581007ca6eec4893982f9438af
nayan kalita <nayan.kalita@inmobi.com>
2012-06-14 14:20:55 +0530

    adding fast-cgi

--- cookbooks/inmobi_fastcgi/metadata.rb
+++ cookbooks/inmobi_fastcgi/metadata.rb
@@ -4,3 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures inmobi_fastcgi"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends "rightscale"
+
+recipe  "inmobi_fastcgi::default", "Installs fast cgi server."
