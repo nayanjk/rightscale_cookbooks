470d1a67346483c1b7cd6aa275a2f4c25d0084fa
nayan kalita <nayan.kalita@inmobi.com>
2012-06-20 19:47:23 +0530

    adding admin app

--- cookbooks/app_admin/recipes/debinst.rb
+++ cookbooks/app_admin/recipes/debinst.rb
@@ -1,18 +1,6 @@
 rightscale_marker :begin
 
 
-if node[:app_admin][:stopcmd] == nil || node[:app_admin][:startcmd] == nil
-   node[:app_admin][:startcmd] = "/etc/init.d/#{node[:app_admin][:service]} start"
-   node[:app_admin][:stopcmd] = "/etc/init.d/#{node[:app_admin][:service]} stop"
-end
-
-service "#{node[:app_admin][:service]}" do
-  start_command "#{node[:app_admin][:startcmd]}"
-  stop_command "#{node[:app_admin][:stopcmd]}"
-  action :nothing
-end
-
-
 
   packages = node[:app_admin][:django_debians]
   packages.gsub(/\s+/, "").split(",").uniq.each do |p|
@@ -26,13 +14,28 @@ end
        log "installing #{p} #{v}"
        package p do
           version "#{v}"
-          notifies :restart , resources(:service => "#{node[:app_admin][:service]}") unless node[:app_admin][:restart] == "false"
        end
     else
        log "Package is #{p} and version is not defined"
        package p
-       notifies :restart , resources(:service => "#{node[:app_admin][:service]}") unless node[:app_admin][:restart] == "false"
     end
   end
+
+   if ( node[:app_admin][:restart] == "true" )
+   service "django" do
+   action :restart
+   end
+   else
+      if ( node[:app_admin][:restart] == "false" )
+           service "django" do
+           action :start
+           end
+       else
+             service "django" do
+             action :nothing
+             end
+      end
+  end 
+  
 
 rightscale_marker :end
