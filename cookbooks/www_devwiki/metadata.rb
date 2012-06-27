7261e6b676493c07512822c9558be2a772cbfd92
nayan kalita <nayan.kalita@inmobi.com>
2012-06-27 14:54:52 +0530

    updated dev wiki recipe

--- cookbooks/www_devwiki/metadata.rb
+++ cookbooks/www_devwiki/metadata.rb
@@ -4,3 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures www_devwiki"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+
+
+recipe "www_devwiki::default", "Installs dev wiki"
