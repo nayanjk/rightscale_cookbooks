9f4cb46562b4a8cb0d6ae3ae6be990445e4b15c6
nayan kalita <nayan.kalita@inmobi.com>
2012-06-27 16:51:35 +0530

    updated dev wiki recipe

--- cookbooks/www_devwiki/resources/default.rb
+++ cookbooks/www_devwiki/resources/default.rb
@@ -18,7 +18,7 @@ actions :stop
 # Runs application server restart sequence
 actions :restart
 
-actions :install_wpmu
+actions :install_devwpmu
 
 # Updates application source files from the remote repository
 # Action designed to setup APP LWRP with common parameters required for source code update/download
