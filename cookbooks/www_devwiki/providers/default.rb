78d2cbc54685999cddf6c2fb3eb331b7ff67819a
nayan kalita <nayan.kalita@inmobi.com>
2012-06-27 14:42:22 +0530

    updated dev wiki recipe

---
+++ cookbooks/www_devwiki/providers/default.rb
@@ -0,0 +1,95 @@
+
+# Cookbook Name:: inmobi_wpsite
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# Stop services
+
+action :stop do
+  log "  Running stop sequence"
+  service "fast_cgi" do
+    action :stop
+    persist false
+  end
+end
+
+# Start services
+action :start do
+  log "  Running start sequence"
+  service "fast_cgi" do
+    action :start
+    persist false
+  end
+
+end
+
+# Restart services
+action :restart do
+  log "  Running restart sequence"
+  action_stop
+     sleep 5
+  action_start
+end
+
+#Installing specified packages
+
+action :install do
+  log " Running apt-get update"
+  packages = new_resource.packages
+  execute "update apt cache" do
+    command "apt-get update"
+    ignore_failure true
+  end
+
+log " Packages which will be installed: #{packages}"
+  v = ""
+  packages.each do |p|
+    if ( p =~ /(.*)=(.*)/ )
+       log "Version defined in #{p} so spliting"
+       p = $1
+       v = $2
+       log "Package is #{p} and version #{v}"
+       log "installing #{p} #{v}"
+       package p do
+          version "#{v}"
+          options "--force-yes"
+       end
+    else
+       log "Package is #{p} and version is not defined"
+       package p do
+       options "--force-yes"
+       end
+    end
+  end
+end
+
+
+
+
+
+
+
+action :install_wpmu do
+   log "copying wpmu.tar.gz file from apppkg1.wc1.inmobi.com"
+
+     remote_file "/tmp/developer_wiki.tar.gz" do
+     source "http://apppkg1.wc1.inmobi.com/developer_wiki.tar.gz"
+     end
+
+     
+     remote_file "/tmp/developer_wiki.sql" do
+     source "http://apppkg1.wc1.inmobi.com/developer_wiki.sql"
+     end
+
+
+     log "extracting wpmu"
+    
+     execute "extract_wpmu" do
+     command <<-COMMAND
+     tar -zxf developer_wiki.tar.gz -C /usr/local/html/
+     COMMAND
+     end
+    
+end
---
+++ cookbooks/www_devwiki/recipes/default.rb
@@ -0,0 +1,86 @@
+
+# Cookbook Name:: inmobi_wpsite
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+rightscale_marker :begin
+
+
+execute "down svc" do
+  command "svc -d /etc/service-apt-update"
+  action :run
+end
+
+log "setting up required directory to host wpmu file"
+
+directory "/usr/local/html" do
+  owner "root"
+  mode 0755
+  recursive true
+  end
+
+
+
+inmobi_wpsite "install wpmu" do
+   persist true
+   action :install_wpmu
+   end
+
+
+log "setting preseed for mysql install"
+
+template "/tmp/set_preseed_mysql.sh" do
+   source "set_preseed_mysql.sh.erb"
+   owner "root"
+   mode "0755"
+end
+
+script "setting mysql preseed" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  ./set_preseed_mysql.sh
+  EOH
+end
+
+
+
+node[:inmobi_wpsite][:packages] = [
+   "mysql-server",
+   "mkhoj-base",
+   "php5-cgi",
+   "php5-mysql",
+  ]
+
+log "installing mysql package"
+
+
+inmobi_wpsite "install_packages" do
+   persist true
+   packages node[:inmobi_wpsite][:packages]
+   action :install
+   end
+
+log "setting required mysql data for wpmu"
+
+template "/tmp/setup_mysql_wpmu.sh" do
+   source "setup_mysql_wpmu.sh.erb"
+   owner "root"
+   mode "0755"
+   end
+
+script "setting mysql data wpmu" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  ./setup_mysql_wpmu.sh
+  EOH
+end
+
+rightscale_marker :end
