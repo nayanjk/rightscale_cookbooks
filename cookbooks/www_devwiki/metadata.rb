a0f3263ac7173a154d998335069e6df01ba199bb
nayan kalita <nayan.kalita@inmobi.com>
2012-06-27 15:34:56 +0530

    updated dev wiki recipe

--- cookbooks/www_devwiki/metadata.rb
+++ cookbooks/www_devwiki/metadata.rb
@@ -8,3 +8,12 @@ version          "0.0.1"
 
 
 recipe "www_devwiki::default", "Installs dev wiki"
+
+attribute "www_devwiki/mysql_root_password",
+  :display_name => "Mysql Server Root Password",
+  :description => "enter mysql root password here",
+  :type => "string",
+  :recipies => ["www_devwiki:default"],
+  :required => "required"
+
+
