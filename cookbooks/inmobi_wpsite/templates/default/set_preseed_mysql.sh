259ad31a22bb43a90491b151e311403a44f2ceda
nayan kalita <nayan.kalita@inmobi.com>
2013-01-01 12:58:07 +0530

    aaaa

--- cookbooks/inmobi_wpsite/templates/default/set_preseed_mysql.sh
+++
@@ -1,5 +0,0 @@
-
-echo "mysql-server-5.1 mysql-server/root_password password <%= node[:inmobi_wpsite][:mysql_root_password] %>" > /tmp/preseed.tmp
-echo "mysql-server-5.1 mysql-server/root_password_again password <%= node[:inmobi_wpsite][:mysql_root_password] %>" >> /tmp/preseed.tmp
-echo "mysql-server-5.1 mysql-server/start_on_boot boolean true" >> /tmp/preseed.tmp
-cat /tmp/preseed.tmp | debconf-set-selections
